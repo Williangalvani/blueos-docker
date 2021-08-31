9da2c182f8f1a5318316a4cf20b244d5f74b5709
Willian Galvani <williangalvani@gmail.com>
2021-08-30 21:42:25 -0300

    fixup! core: tools: add code-server bootstrap

--- core/tools/code-server/bootstrap.sh
+++ core/tools/code-server/bootstrap.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-apt update && apt install -y curl
-curl -fsSL https://code-server.dev/install.sh | sh
+apt update && apt install -y curl libatomic1
+
+if [[ "$(uname -m)" != "x86_64"* ]]; then
+
+    wget https://nodejs.org/dist/v14.16.1/node-v14.16.1-linux-armv7l.tar.gz
+    tar -xzf node-v14.16.1-linux-armv7l.tar.gz
+    cp -R node-v14.16.1-linux-armv7l/* /usr/local/
+    rm -r node-v14.16.1-linux-armv7l*
+    npm install -g code-server
+fi
+
+if [[ "$(uname -m)" == "x86_64"* ]]; then
+    curl -fsSL https://code-server.dev/install.sh | sh
+fi
+rm -r /root/.config/code-server || true
