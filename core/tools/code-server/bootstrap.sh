ff11de503580937ab620c18e7e06b94ac6f55f9a
Willian Galvani <williangalvani@gmail.com>
2021-08-30 23:31:14 -0300

    works

--- core/tools/code-server/bootstrap.sh
+++ core/tools/code-server/bootstrap.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-apt update && apt install -y curl libatomic1
+
+BUILD_PACKAGES=(
+    build-essential
+    pkg-config
+    git
+    jq
+    curl
+)
+
+
+apt update && apt install --no-install-recommends -y ${BUILD_PACKAGES[*]} libx11-dev libxkbfile-dev libsecret-1-dev libatomic1
 
 if [[ "$(uname -m)" != "x86_64"* ]]; then
 
@@ -8,10 +18,14 @@ if [[ "$(uname -m)" != "x86_64"* ]]; then
     tar -xzf node-v14.16.1-linux-armv7l.tar.gz
     cp -R node-v14.16.1-linux-armv7l/* /usr/local/
     rm -r node-v14.16.1-linux-armv7l*
-    npm install -g code-server
+    npm install -g code-server --unsafe-perm
 fi
 
 if [[ "$(uname -m)" == "x86_64"* ]]; then
     curl -fsSL https://code-server.dev/install.sh | sh
 fi
 rm -r /root/.config/code-server || true
+
+apt -y remove ${BUILD_PACKAGES[*]}
+apt -y autoremove
+apt -y clean
