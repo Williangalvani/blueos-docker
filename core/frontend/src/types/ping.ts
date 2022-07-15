4470a2aa346668ce50959863248b78a80ec036db
Willian Galvani <williangalvani@gmail.com>
2022-07-15 16:41:17 -0300

    tweaks

--- core/frontend/src/types/ping.ts
+++ core/frontend/src/types/ping.ts
@@ -1,6 +1,4 @@
-import { Baudrate } from '@/types/common'
-
-export interface PingDeviceInterface {
+export interface PingDevice {
   ping_type: string,
   device_id: number,
   device_model: number,
@@ -9,21 +7,4 @@ export interface PingDeviceInterface {
   firmware_version_minor: number,
   firmware_version_patch: number,
   port: string,
-}
-
-export class PingDevice implements PingDeviceInterface {
-  constructor(
-    public readonly ping_type: string,
-    public readonly device_id: number,
-    public readonly device_model: number,
-    public readonly device_revision: number,
-    public readonly firmware_version_major: number,
-    public readonly firmware_version_minor: number,
-    public readonly firmware_version_patch: number,
-    public readonly port: string,
-  ) {}
-
-  fw_version(): string {
-    return `${this.firmware_version_major}.${this.firmware_version_minor}.${this.firmware_version_patch}`
-  }
 }
--- core/frontend/src/views/Peripherals.vue
+++ core/frontend/src/views/Peripherals.vue
@@ -6,7 +6,11 @@
       no-gutters
     />
     <v-card>
-      <h2>Ping Devices</h2>
+      <v-card-title class="justify-center">
+        <h3>
+          Ping Devices
+        </h3>
+      </v-card-title>
       <v-row>
         <v-col
           v-for="ping in ping_devices"
@@ -14,15 +18,31 @@
         >
           <v-card
             class="py-3 px-3"
-            style="min-height: 100%; max-width: 30%;"
+            style="min-height: 100%; max-width: 40%;"
           >
-            <h4>{{ ping.ping_type }}</h4>
+            <v-card-title class="justify-center">
+              {{ ping.ping_type }}
+            </v-card-title>
             <v-simple-table dense>
               <tbody>
+                <tr>
+                  <td>Firmware</td><td>
+                    {{ `${ping.firmware_version_major}.${ping.firmware_version_minor}.${ping.firmware_version_patch}` }}
+                  </td>
+                </tr>
                 <tr><td>ID</td><td>{{ ping.device_id }}</td></tr>
                 <tr><td>Model</td><td>{{ ping.device_model }}</td></tr>
                 <tr><td>Revision</td><td>{{ ping.device_revision }}</td></tr>
-                <tr><td>Port</td><td>{{ ping.port }}</td></tr>
+                <tr><td>Device</td><td>{{ ping.port }}</td></tr>
+                <tr><td>Bridge</td><td> N/A </td></tr>
+                <tr>
+                  <td>Mavlink Driver</td><td>
+                    <v-switch
+                      inset
+                      disabled
+                    />
+                  </td>
+                </tr>
               </tbody>
             </v-simple-table>
           </v-card>
@@ -52,6 +72,6 @@ export default Vue.extend({
 
 <style scoped>
   .v-card {
-    padding: 30px;
+    padding: 20px;
   }
 </style>
