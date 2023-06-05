7d7fc783a73573dd9614d4b0541cdd3364de601d
Willian Galvani <williangalvani@gmail.com>
2023-06-05 12:40:47 -0300

    core: frontend: add loading for default parameters

--- core/frontend/src/components/parameter-editor/ParameterLoader.vue
+++ core/frontend/src/components/parameter-editor/ParameterLoader.vue
@@ -73,7 +73,7 @@
           </template>
         </v-progress-linear>
       </v-card-text>
-      <v-card-text v-else-if="writing">
+      <v-card-text v-else-if="write_finished">
         <v-alert
           type="success"
         >
@@ -150,6 +150,9 @@ export default Vue.extend({
     },
     user_selected_params_length(): number {
       return Object.keys(this.user_selected_params).length
+    },
+    write_finished(): boolean {
+      return this.user_selected_params_length === 0
     },
   },
   watch: {
