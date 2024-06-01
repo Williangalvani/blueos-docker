8f49cbbcd8214b988b52b96e9d5241c728dbe6ae
Willian Galvani <williangalvani@gmail.com>
2024-06-01 13:46:15 -0300

    core/services/install-services.sh: blueos_startup_update: add boot-loop detection

--- core/services/install-services.sh
+++ core/services/install-services.sh
@@ -8,10 +8,10 @@ BUILD_PACKAGES=(
 )
 
 # Install build packages if not on armv7, which we have all pre-built wheels for
-if ! { [ "$TARGETARCH" == "arm" ] && [ "$TARGETVARIANT" == "v7" ]; }; then
-    apt update
-    apt install -y --no-install-recommends ${BUILD_PACKAGES[*]}
-fi
+#if ! { [ "$TARGETARCH" == "arm" ] && [ "$TARGETVARIANT" == "v7" ]; }; then
+    #apt update
+    #apt install -y --no-install-recommends ${BUILD_PACKAGES[*]}
+#fi
 
 # Wifi service:
 ## Bind path for wpa
@@ -33,14 +33,15 @@ SERVICES=(
     wifi
 )
 
+pip install packaging
+/home/pi/services/install_dependencies.py
 # We need to install loguru, appdirs and pydantic since they may be used inside setup.py
-python -m pip install appdirs==1.4.4 loguru==0.5.3 pydantic==1.10.12
 
 for SERVICE in "${SERVICES[@]}"; do
     echo "Installing service: $SERVICE"
     cd "/home/pi/services/$SERVICE/" && pip3 install .
 done
 
-apt -y remove ${BUILD_PACKAGES[*]}
-apt -y autoremove
-apt -y clean
+#apt -y remove ${BUILD_PACKAGES[*]}
+#apt -y autoremove
+#apt -y clean
